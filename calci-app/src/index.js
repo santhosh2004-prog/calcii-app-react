54b8313dad589cd618d2d4d85c88a97bd9b63aa5
santhosh <gundasanthosh55@gmail.com>
2025-05-30 13:45:27 +0530

    new update

--- calci-app/src/index.js
+++ calci-app/src/index.js
@@ -8,12 +8,10 @@ import Searchbar from './Searchbar';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <h1>This is calci project</h1>
     <App />
+    
     
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
